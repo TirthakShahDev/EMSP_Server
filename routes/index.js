7d1852936ffe09bfd075a92ee97cda6c59b08ed0
Tirthak Shah <tirthak.it@outlook.com>
2020-04-17 17:20:47 +0530

    cfghg

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const uuid = require("uuid");
 
-const PUBLIC_URL = "https://emsp.herokuapp.com";
+const PUBLIC_URL = "https://emsp.herokurouter.com";
 const TOKEN_B = uuid.v4();
 console.log("Auth TOKEN_B = " + TOKEN_B);
 
@@ -50,6 +50,57 @@ router.get("/ocpi/2.2", authorize, async (_, res) => {
 		},
 		timestamp: new Date()
 	})
+})
+
+
+router.get("/ocpi/2.2", authorize, async (req, res) => {
+    res.send({
+        status_code: 1000,
+        data: {
+            version: "2.2",
+            endpoints: [
+                {
+                    "identifier": "cdrs",
+                    "role": "RECEIVER",
+                    "url": `${PUBLIC_URL}/ocpi/emsp/2.2/cdrs`
+                },
+                {
+                    "identifier": "commands",
+                    "role": "SENDER",
+                    "url": `${PUBLIC_URL}/ocpi/emsp/2.2/commands`
+                }
+            ]
+        },
+        timestamp: new Date()
+    })
+})
+
+router.post("/ocpi/emsp/2.2/commands/:command/:uid", authorize, async (req, res) => {
+    console.log(`EMSP [DE MSP] received async command response: ${JSON.stringify(req.body)}`)
+    res.send({
+        status_code: 1000,
+        timestamp: new Date()
+    })
+})
+
+let cdr
+
+router.get("/ocpi/emsp/2.2/cdrs/1", authorize, async (req, res) => {
+    res.send({
+        status_code: 1000,
+        data: cdr,
+        timestamp: new Date()
+    })
+})
+
+router.post("/ocpi/emsp/2.2/cdrs", authorize, async (req, res) => {
+    cdr = req.body
+    res.set({
+        "Location": `${PUBLIC_URL}/ocpi/emsp/2.2/cdrs/1`
+    }).send({
+        status_code: 1000,
+        timestamp: new Date()
+    })
 })
 
 module.exports = router;
